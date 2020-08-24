59207e86af4e82468d455a346dcbcd3e945d8142
Julian Shalaby <jshalaby@ucsc.edu>
2020-08-23 17:51:14 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TicTacToe from "./TicTacToe/components/TicTacToe";
-import './TicTacToe/CSS/TicTacToe.css'
-
 import RandomColorPicker from "./RandomColorPicker/Components/RandomColorPicker";
 import './RandomColorPicker/CSS/RandomColorPicker.css'
 
