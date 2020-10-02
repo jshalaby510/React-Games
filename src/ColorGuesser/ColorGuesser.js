e5388ff44e73f5d6e35b3b6af6757c5f9a16926a
Julian Shalaby <julianshalaby@gmail.com>
2020-10-02 11:31:46 -0700

    temp disabled game

---
+++ src/ColorGuesser/ColorGuesser.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Board from "./Components/Board";
+import NavBar from "../NavBar";
+import RandomColor from 'randomcolor';
+
+function initialize () {
+    const boxes = [];
+    for (let i=0; i<9; i++){
+        boxes.push({
+            id: i,
+            color: RandomColor()
+        });
+    }
+    return boxes;
+}
+
+const randomNumber = Math.floor(Math.random() * 9);
+
+function ColorGuesser() {
+    const [boxes, setBoxes] = useState(initialize());
+    const [banner, setBanner] = useState(`Which color is: ${boxes[randomNumber].color}?`);
+
+    const handleClick = (i) => {
+        if(i === randomNumber) setBanner("You win!")
+    }
+
+    return (
+        <>
+            <NavBar/>
+            <div className={"gameBody"}>
+                <h1 className={"rainbow-text"}>{banner}</h1>
+                <Board
+                    boxes={boxes}
+                    handleClick={handleClick}
+                />
+            </div>
+        </>
+  );
+
+}
+
+export default ColorGuesser;
